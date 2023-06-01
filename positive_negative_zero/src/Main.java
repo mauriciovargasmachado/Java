32708a740cc6d1323ccda6898f5f20e679d4c054
Mauricio Vargas <mauriciovargasmachado@gmail.com>
2023-06-01 16:40:17 -0400

    2023-06-01/1

---
+++ positive_negative_zero/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    int number = Integer.parseInt(JOptionPane.showInputDialog("please type a number: "));
+        public static void checkNumber(int number){
+
+            if (number>0){
+                System.out.println("positive");
+            }
+            else if(number<0){
+                System.out.println("negative");
+            }
+            else {
+                System.out.println("zero");
+            }
+        }
+
+}
